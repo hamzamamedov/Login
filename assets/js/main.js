037f67280a2b2bf93f1929d6306bb77b71c2ed48
hamzamamedov <hemze.mamedov@mail.ru>
2024-06-03 18:56:33 +0400

    changed html and add js code

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+function login(){
+    let x,y;
+    x=document.getElementById("username").value;
+    y=document.getElementById("password").value;
+
+    if(y=="javascript"){
+        alert("Tebrikler daxil oldunuz ...");
+    }else{
+        alert("Parolunuz duzgun deyil !!!");
+    }
+}
