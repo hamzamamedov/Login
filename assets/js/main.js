f311e7fbf6b99d00b75c8232ca3fd34cebe2227a
hamzamamedov <hemze.mamedov@mail.ru>
2024-06-03 19:13:50 +0400

    add  some js code and html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,13 @@
-function login(){
-    let x,y;
-    x=document.getElementById("username").value;
-    y=document.getElementById("password").value;
+function login() {
+  let x, y;
+  x = document.getElementById("username").value;
+  y = document.getElementById("password").value;
 
-    if(y=="javascript"){
-        alert("Tebrikler daxil oldunuz ...");
-    }else{
-        alert("Parolunuz duzgun deyil !!!");
-    }
+  if (y == "javascript") {
+    alert("Tebrikler daxil oldunuz ...");
+    document.getElementById("myform").action = "https://mashin.al/";
+  } else {
+    alert("Parolunuz duzgun deyil !!!");
+    document.getElementById("myform").action="";
+  }
 }
